07dc1558c56d1d785641da5387bbde528af3b87b
Haobin Qu <hq617@york.ac.uk>
2020-06-22 20:41:55 +0100

    Refactor to break up MainWindow into smaller classes

---
+++ Timesheet remainder V2/ExcelManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+
+namespace Timesheet_remainder
+{
+    public class ExcelManager
+    {
+        public void NewExcelFile(string fileLoadPath)
+        {
+            var fileInfoPath = new FileInfo(fileLoadPath);
+            using (var excelPackage = new ExcelPackage(fileInfoPath))
+            {
+                var worksheet = excelPackage.Workbook.Worksheets.Add("MySheet");
+                SetNewHeader(worksheet);
+
+                excelPackage.Save();
+            }
+        }
+
+        public void AddNewEntryToWorkSheet(string fileLoadPath, DateTime sheetDateTime, string TaskInputText)
+        {
+            var fileInfoPath = new FileInfo(fileLoadPath);
+            using (var excelPackage = new ExcelPackage(fileInfoPath))
+            {
+                var worksheet = excelPackage.Workbook.Worksheets["MySheet"];
+                var lastRow = worksheet.Dimension.End.Row;
+                var lastColumn = worksheet.Dimension.End.Column;
+
+                //Set the next last cell in the row to sheetDate.Text
+                worksheet.Cells[lastRow + 1, 1].Value = sheetDateTime.ToString("dd/MM/yy");
+                //Set the next last cell in the row to txtTaskInput.Text;
+                worksheet.Cells[lastRow + 1, 2].Value = sheetDateTime.ToString("HH:mm:ss");
+                worksheet.Cells[lastRow + 1, 3].Value = TaskInputText;
+
+                excelPackage.Save();
+            }
+        }
+
+        private void SetNewHeader(ExcelWorksheet worksheet)
+        {
+            worksheet.Cells["A1"].Value = "Date";
+            worksheet.Cells["B1"].Value = "Time";
+            worksheet.Cells["C1"].Value = "Task Description";
+        }
+    }
+}
--- Timesheet remainder V2/MainWindow.xaml.cs
+++ Timesheet remainder V2/MainWindow.xaml.cs
@@ -17,7 +17,6 @@ using System.Windows.Shapes;
 using System.IO;
 using OfficeOpenXml;
 using System.Drawing;
-using System.Timers;
 using System.Data;
 
 
@@ -28,13 +27,10 @@ namespace Timesheet_remainder
     /// </summary>
     public partial class MainWindow : Window
     {
-        string loadPath = string.Empty; //new file file path
-        string sheetDay = DateTime.Now.ToString("dd/MM/yy");
-        string sheetTime = DateTime.Now.ToString("HH:mm:ss");
-
-        int popupCounter = 0;
-
-        private static Timer aTimer;
+        private string _fileLoadPath = string.Empty; //new file file path
+        private DateTime _sheetDateTime;
+        private int _popupCounter = 0;
+        private readonly ExcelManager _excelManager = new ExcelManager();
 
         public MainWindow()
         {
@@ -43,6 +39,7 @@ namespace Timesheet_remainder
 
             //Sets sheetDate text block to the current time
             sheetDate.Text = DateTime.Now.ToString("dd/MM/yy   HH:mm");
+            _sheetDateTime = DateTime.Now;
 
 
             //Sets status to no file load
@@ -51,148 +48,128 @@ namespace Timesheet_remainder
             statusMsg.Text = "Timesheet not loaded";
 
 
-            // Create a timer and set a 20 s interval.
-            aTimer = new System.Timers.Timer();
-            aTimer.Interval = 20 * 1000;
+            NewTimer();
 
-            // Hook up the Elapsed event for the timer. 
-            aTimer.Elapsed += OnTimedEvent;
-
-            // Have the timer fire repeated events (true is the default)
-            aTimer.AutoReset = true;
-
-            // Start the timer
-            aTimer.Enabled = true;
-
-
-
-            //Minimise to tray
-            System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon();
-
-            //set the tray icon
-            //ni.Icon = new System.Drawing.Icon(@filePath); //tray icon is in the path
-            ni.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
-            //ni.Icon = new Icon(this.Icon, 40, 40); //trayIcon is your NotifyIcon
-
-            //System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
-            ni.Visible = true;
-
-            //Maximise when double click            
-            ni.Click +=
-                delegate (object sender, EventArgs args)
-                {
-                    this.Show();
-                    this.WindowState = WindowState.Normal;
-                };
+            TrayManagement();
         }
 
-        //State change events minimise to hide window
         protected override void OnStateChanged(EventArgs e)
         {
+            //State change events minimise to hide window
             if (WindowState == System.Windows.WindowState.Minimized)
                 this.Hide();
 
             base.OnStateChanged(e);
         }
 
-        //Timer event
-        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        #region Auto Popup
+        private void NewTimer()
+        {
+            // Create a timer and set a 20 s interval.
+            var timer = new System.Timers.Timer();
+            const int intervalSeconds = 20 * 1000;
+            
+            timer.Interval = intervalSeconds;
+
+            // Hook up the Elapsed event for the timer. 
+            timer.Elapsed += TimedPopupEvent;
+
+            // Have the timer fire repeated events (true is the default)
+            timer.AutoReset = true;
+
+            // Start the timer
+            timer.Enabled = true;
+        }
+
+        private void TimedPopupEvent(Object source, System.Timers.ElapsedEventArgs e)
         {
             //Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);  //use for debug
             DateTime currentTime = DateTime.Now;
             
-
             this.Dispatcher.Invoke(() =>
             {
                 sheetDate.Text = DateTime.Now.ToString("dd/MM/yy   HH:mm");
-                sheetDay = DateTime.Now.ToString("dd/MM/yyyy");
-                sheetTime = DateTime.Now.ToString("HH:mm:ss");
+                _sheetDateTime = DateTime.Now;
+
                 //Autopopup at set minutes
                 if (currentTime.Minute == 30 | currentTime.Minute == 00)
                 {                    
-                    if (popupCounter == 0)
+                    if (_popupCounter == 0)
                     {                        
                         this.Show();
                         this.WindowState = WindowState.Normal;                       
-                        popupCounter += 1;
-                        Console.WriteLine("autopopup triggered, counter= " + popupCounter.ToString() + ", Time = " + currentTime.Minute.ToString()); //debug
+                        _popupCounter += 1;
+
+                        var debugString =
+                            $"timed popup triggered at Time = {currentTime.Minute}, counter = {_popupCounter}"; 
+                        Console.WriteLine(debugString);
                     }                                                           
                     //Console.WriteLine(counter.ToString()); //debug output for 
                 } 
                 else
                 {
-                    popupCounter = 0;
+                    _popupCounter = 0;
                 }             
             });
         }
 
-        //buttons
+
+        #endregion
+
+        private void TrayManagement()
+        {
+            //Minimise to tray
+            var notifyIcon = new System.Windows.Forms.NotifyIcon
+            {
+                Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath),
+                Visible = true
+            };
+
+            //Maximise when double click            
+            notifyIcon.Click +=
+                delegate (object sender, EventArgs args)
+                {
+                    this.Show();
+                    _sheetDateTime = DateTime.Now;
+                    this.WindowState = WindowState.Normal;
+                };
+        }
+
+        #region Buttons
         private void btnNewSheet_Click(object sender, RoutedEventArgs e)
         {
-            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            var newExcelSheetDialogue = new Microsoft.Win32.SaveFileDialog {DefaultExt = ".xlsx"};
 
-            dlg.DefaultExt = ".xlsx";
-
-            Nullable<bool> result = dlg.ShowDialog();
-
-            if (result == true)
+            var newExcelSheetDialogueSuccessful = newExcelSheetDialogue.ShowDialog();
+            if (newExcelSheetDialogueSuccessful == true)
             {
-                // Open document 
-                loadPath = dlg.FileName;
+                _fileLoadPath = newExcelSheetDialogue.FileName;
             }
 
-            Console.WriteLine(loadPath); //use for debug
-
-            if (dlg.FileName != "")
+            if (newExcelSheetDialogue.FileName != string.Empty)
             {
-                var fi = new FileInfo(@loadPath);
-                using (var p = new ExcelPackage(fi))
+                try
                 {
-                    //Get the Worksheet created in the previous codesample. 
-                    try
-                    {
-                        var ws = p.Workbook.Worksheets.Add("MySheet");
-
-
-                        //Set headers
-                        ws.Cells["A1"].Value = "Date";
-                        ws.Cells["B1"].Value = "Time";
-                        ws.Cells["C1"].Value = "Task Description";
-
-                        p.Save();
-
-                        statusMsg.Text = string.Empty; //removes timesheet error msg
-                    }
-                    catch
-                    {
-                        statusMsg.Foreground = System.Windows.Media.Brushes.Red;
-                        statusMsg.Text = "Invalid operation";
-                    }
-
+                    _excelManager.NewExcelFile(_fileLoadPath);
+                    statusMsg.Text = string.Empty;
                 }
-
-                /*               // Saves the Image via a FileStream created by the OpenFile method.
-                               System.IO.FileStream fs =
-                                   (System.IO.FileStream)dlg.OpenFile();
-                               fs.Close();*/
+                catch
+                {
+                    statusMsg.Foreground = System.Windows.Media.Brushes.Red;
+                    statusMsg.Text = "Invalid operation";
+                }
             }
         }
 
         private void btnLoadSheet_Click(object sender, RoutedEventArgs e)
         {
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog {DefaultExt = ".xlsx"};
 
-            // Set filter for file extension and default file extension 
-            dlg.DefaultExt = ".xlsx";
+            bool? showDialogSuccessful = openFileDialog.ShowDialog();
 
-            // Display OpenFileDialog by calling ShowDialog method 
-            Nullable<bool> result = dlg.ShowDialog();
-
-            // Get the selected file name and display in a TextBox 
-            if (result == true)
+            if (showDialogSuccessful == true)
             {
-                // Open document 
-                loadPath = dlg.FileName;
+                _fileLoadPath = openFileDialog.FileName;
                 statusMsg.Text = string.Empty;
             }
 
@@ -200,29 +177,17 @@ namespace Timesheet_remainder
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
-            if (loadPath != string.Empty && statusMsg.Text != "Invalid operation")
+            if (_fileLoadPath != string.Empty && statusMsg.Text != "Invalid operation")
             {
-                var fi = new FileInfo(@loadPath);
+                var fileInfoPath = new FileInfo(_fileLoadPath);
 
-                using (var p = new ExcelPackage(fi))
+                using (var excelPackage = new ExcelPackage(fileInfoPath))
                 {
 
                     try
                     {
-                        //Get the Worksheet created in the previous codesample. 
-                        var ws = p.Workbook.Worksheets["MySheet"];
-                        var lastRow = ws.Dimension.End.Row;
-                        var lastColumn = ws.Dimension.End.Column;
-
-                        //Set the next last cell in the row to sheetDate.Text
-                        ws.Cells[lastRow + 1, 1].Value = sheetDay;
-                        //Set the next last cell in the row to txtTaskInput.Text;
-                        ws.Cells[lastRow + 1, 2].Value = sheetTime;
-                        ws.Cells[lastRow + 1, 3].Value = txtTaskInput.Text;
-
-                        p.Save();
+                        _excelManager.AddNewEntryToWorkSheet(_fileLoadPath, _sheetDateTime, txtTaskInput.Text);
                         statusMsg.Text = string.Empty;
-
                         this.WindowState = WindowState.Minimized;
                     }
                     catch
@@ -240,26 +205,13 @@ namespace Timesheet_remainder
             this.WindowState = WindowState.Minimized;
         }
 
-/*        public class Tasks
-        {
-            public string CalcTask { get; set; }
-            public double CalcTime { get; set; }
-
-            public Tasks(string calcTask = "No Name", double calcTime = 0) //constructor
-            {
-                CalcTask = calcTask;
-                CalcTime = calcTime;
-            }    
-        }*/
-
-
         private void btnCalc_Click(object sender, RoutedEventArgs e)
         {
-            if (loadPath != string.Empty && statusMsg.Text != "Invalid operation")
+            if (_fileLoadPath != string.Empty && statusMsg.Text != "Invalid operation")
             {
                 try
                 {
-                    var fi = new FileInfo(@loadPath);
+                    var fi = new FileInfo(_fileLoadPath);
                     using (var p = new ExcelPackage(fi))
                     {
 
@@ -278,44 +230,6 @@ namespace Timesheet_remainder
                             instanceList.Add(wsTimes.Cells[row, 3].Value);
                         }
 
-
-                        //mainly used for debug not output to excel file
-                        //List<Object> calcList = new List<Object>();
-                        /*                    string taskDate;
-                                            string nextTaskDate;
-                                            string taskTime;
-                                            string nextTaskTime;
-                                            TimeSpan calcTaskTime;
-                                            string taskDesc;
-                                            string nextTaskDesc;
-                                            */
-                        /*                    for (int order = 0; order <= instanceList.Count; order += 3)
-                                            {
-
-
-                                                if (instanceList.Count >= order + 5)
-                                                {
-                                                    taskDate = (string)instanceList[order];
-                                                    taskTime = (string)instanceList[order + 1];
-                                                    taskDesc = (string)instanceList[order + 2];
-                                                    nextTaskDate = (string)instanceList[order + 3];
-                                                    nextTaskTime = (string)instanceList[order + 4];
-                                                    nextTaskDesc = (string)instanceList[order + 5];
-
-                                                    if (nextTaskDate != null && nextTaskTime != null && nextTaskDesc != null)
-                                                    {
-                                                        //if  (taskDesc == nextTaskDesc && taskDate == nextTaskDate)
-                                                        if (taskDate == nextTaskDate)
-                                                        {
-                                                            calcList.Add(taskDesc);
-                                                            calcTaskTime = DateTime.Parse(nextTaskTime).Subtract(DateTime.Parse(taskTime));
-                                                            calcList.Add(calcTaskTime);
-                                                            Console.WriteLine("taskDecs =" + taskDesc + " calcTaskTime =" + calcTaskTime);
-                                                        }
-                                                    }
-                                                }
-                                            }*/
-
                         DataTable inputTable = new DataTable("inputTable1");
                         inputTable.Clear();
                         inputTable.Columns.Add("Date");
@@ -329,7 +243,6 @@ namespace Timesheet_remainder
                             _task["TaskDesc"] = Convert.ToString(instanceList[order + 2]);
                             inputTable.Rows.Add(_task);
                         }
-                        //inputTable.WriteXml(@"C:\Users\Ben\Desktop\Timesheet test\calcTest\inputTable.xml");
 
                         DataTable calcTable = new DataTable("calcTable1");
                         calcTable.Clear();
@@ -435,12 +348,13 @@ namespace Timesheet_remainder
                 }
 
             }            
-             else
+            else
             {
                 statusMsg.Foreground = System.Windows.Media.Brushes.Red;
                 statusMsg.Text = "Invalid operation";
             }
         }
+        #endregion
     }
 
 }
